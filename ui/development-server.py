2232f1a4bad1b0e4fee44e365abb5c764024a858
Rajeev Vadhavkar <rajeev22.18@gmail.com>
2024-10-04 15:14:27 -0500

    update dev server script to generate keys if not present. add comments to html

--- ui/development-server.py
+++ ui/development-server.py
@@ -4,6 +4,8 @@ import ssl
 
 httpd = HTTPServer(('127.0.0.1', 5000), SimpleHTTPRequestHandler)
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+if not (os.path.exists('cert.pem') and os.path.exists('key.pem')):
+    subprocess.run(["mkcert", "-cert-file", "cert.pem", "-key-file", "key.pem", "localhost"], check=True)
 context.load_cert_chain(certfile='cert.pem', keyfile='key.pem', password='')
 context.set_ciphers("@SECLEVEL=1:ALL")
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
